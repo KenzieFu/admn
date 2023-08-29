32ba63f790a89c884049c8d02bee76599816e3ba
LAPTOP-B0212VLT\User <kenziefubrianto@gmail.com>
2023-08-29 20:29:10 +0700

    no message

--- lib/constaints.dart
+++ lib/constaints.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const textColor = Color(0xff585858);
-const primary = Color(0xff89B5A2);
+const primary = Color(0xFF171821);
 const primaryLight = Color(0xffCCECDF);
 const primaryAncient = Color(0xff618777);
 
