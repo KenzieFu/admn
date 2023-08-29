7e0662e5702b5fda0b7ff5bce4e29dcba3c56cff
ivandohan <srgivando@gmail.com>
2023-08-29 21:46:01 +0700

    Updated Dashboard Components

--- lib/components/status_list.dart
+++ lib/components/status_list.dart
@@ -6,10 +6,10 @@ import 'package:store_responsive_dashboard/widgets/status_card.dart';
 import '../constaints.dart';
 
 final List<BussinessStatus> statusList = [
-  BussinessStatus('Total Transaction', '1123456 \$', Icons.show_chart_outlined),
-  BussinessStatus('Total Loan', '11234 \$', Icons.attach_money_outlined),
-  BussinessStatus('Total Items', '1236', Icons.shopping_cart_outlined),
-  BussinessStatus('Active Users', '11234', Icons.people_outline_outlined),
+  BussinessStatus('Profit Lelang', 'Rp 1.234.567.556', Icons.show_chart_outlined),
+  BussinessStatus('Total Loan', 'Rp 2.238.324.567', Icons.attach_money_outlined),
+  BussinessStatus('Market Store', '337', Icons.shopping_cart_outlined),
+  BussinessStatus('Donatur', '10', Icons.people_outline_outlined),
 ];
 
 class StatusList extends StatelessWidget {
